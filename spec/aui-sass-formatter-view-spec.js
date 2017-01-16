23c058fe7350d1eccdb871c85024d83e8694df24
Alvis <notinkham@gmail.com>
2017-01-16 22:05:38 +0200

    init

---
+++ spec/aui-sass-formatter-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AuiSassFormatterView from '../lib/aui-sass-formatter-view';
+
+describe('AuiSassFormatterView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
